77ba8aa40265cd14f6ae9d56a171722bf523c4ee
Andre Bastos Morais <andre.bastos@sladm.com.br>
2026-06-11 18:50:35 -0300

    db aceita DATABASE_URL

--- db.js
+++ db.js
@@ -1,15 +1,20 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const pool = new Pool({
-  host:     process.env.DB_HOST,
-  port:     process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user:     process.env.DB_USER,
-  password: process.env.DB_PASS,
-});
+const pool = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
+    })
+  : new Pool({
+      host:     process.env.DB_HOST,
+      port:     process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      user:     process.env.DB_USER,
+      password: process.env.DB_PASS,
+    });
 
 pool.on("connect", () => console.log("✅ Conectado ao PostgreSQL"));
-pool.on("error",   (err) => console.error("❌ Erro no pool PostgreSQL:", err));
+pool.on("error",   (err) => console.error("❌ Erro:", err));
 
 module.exports = pool;
